614b7a0704dfc0b66336725be44c810d23f6a9c9
Quan Teng Foong <tengfoong.quan@shopee.com>
2025-02-26 18:21:32 +0800

    add mini.animate plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -827,6 +827,29 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+
+  {
+    'echasnovski/mini.animate',
+    lazy = false,
+    version = '*',
+    config = function()
+      local animate = require 'mini.animate'
+      animate.setup {
+        cursor = {
+          timing = animate.gen_timing.linear { duration = 10, unit = 'step' },
+          -- path = animate.gen_path.line {
+          --   predicate = function()
+          --     return true -- always animate, even if cursor in the same line
+          --   end,
+          -- },
+        },
+        scroll = {
+          timing = animate.gen_timing.linear { duration = 5, unit = 'step' },
+          subscroll = animate.gen_subscroll.equal { max_output_steps = 120 }, -- number of steps in animation (smoothness)
+        },
+      }
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
