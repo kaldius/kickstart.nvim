23b4c1c1f753908b4ad2d88ab5e1698dbd098c5d
Quan Teng Foong <tengfoong.quan@shopee.com>
2025-02-26 18:18:25 +0800

    add diagnostics on hover

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Show diagnostics in a pop-up window on hover
+vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+  desc = 'Show diagnostics in a pop-up window on hover',
+  group = vim.api.nvim_create_augroup('reset_group', {}),
+  callback = function()
+    local current_cursor = vim.api.nvim_win_get_cursor(0)
+    local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
+
+    -- Show the popup diagnostics window,
+    -- but only once for the current cursor location (unless moved afterwards).
+    if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
+      vim.w.lsp_diagnostics_last_cursor = current_cursor
+      vim.diagnostic.open_float(nil, {
+        scope = 'cursor', -- "cursor"/"line"/"buffer" dictating what diagnostics should show up
+        focusable = false,
+      })
+    end
+  end,
+})
