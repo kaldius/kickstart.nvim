f68572b64d22ceab6c5854ad31e0418a8443f2da
Quan Teng Foong <tengfoong.quan@shopee.com>
2025-02-27 14:15:01 +0800

    add nvim-autopairs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,6 +1,3 @@
--- autopairs
--- https://github.com/windwp/nvim-autopairs
-
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
@@ -8,7 +5,7 @@ return {
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
     require('nvim-autopairs').setup {}
-    -- If you want to automatically add `(` after selecting a function or method
+    -- Automatically add `(` after selecting a function or method
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
