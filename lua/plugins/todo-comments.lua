0b35229efec3e859d18984caec969a98d9c11d59
Quan Teng Foong <tengfoong.quan@shopee.com>
2025-03-05 13:31:42 +0800

    add keymaps for todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,6 +1,25 @@
 return { -- Highlight todo, notes, etc in comments
   'folke/todo-comments.nvim',
   event = 'VimEnter',
+  cmd = { 'TodoTelescope' },
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = { signs = false },
+  keys = {
+    {
+      ']t',
+      function()
+        require('todo-comments').jump_next()
+      end,
+      desc = 'Next Todo Comment',
+    },
+    {
+      '[t',
+      function()
+        require('todo-comments').jump_prev()
+      end,
+      desc = 'Previous Todo Comment',
+    },
+    { '<leader>ft', '<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>', desc = '[F]ind Critical [T]odos' },
+    { '<leader>fT', '<cmd>TodoTelescope<cr>', desc = '[F]ind ALL [T]odos' },
+  },
 }
