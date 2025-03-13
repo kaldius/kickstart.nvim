17883768b9de01748604a987bedb938ec556b600
Quan Teng Foong <e0564970@u.nus.edu>
2025-03-13 21:59:51 +0800

    cleanup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,60 +1,62 @@
 -- [[ Setting options ]]
--- See `:help vim.opt`
+-- See `:help opt`
 -- For more options, you can see `:help option-list`
 
+local opt = vim.opt
+
 -- Make line numbers default
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+opt.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  opt.clipboard = 'unnamedplus'
 end)
 
 -- Enable break indent (wrapped lines will be visually indented)
-vim.opt.breakindent = true
+opt.breakindent = true
 
 -- Save undo history
-vim.opt.undofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes:1'
+opt.signcolumn = 'yes:1'
 
 -- Decrease update time
-vim.opt.updatetime = 250 -- updatetime(ms) is used for writing to swapfile and CursorHold
+opt.updatetime = 250 -- updatetime(ms) is used for writing to swapfile and CursorHold
 
 -- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
+opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 --  Currently disabled to not conflict with indent-blankline
--- vim.opt.list = true
--- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- opt.list = true
+-- opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+opt.inccommand = 'split'
 
 -- Highlight the line number where your cursor is.
-vim.opt.cursorline = true
+opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.scrolloff = 10
--- lua/plugins/neovim-session-manager.lua
+++ lua/plugins/neovim-session-manager.lua
@@ -6,8 +6,8 @@ return {
     local config = require 'session_manager.config'
     require('session_manager').setup {
       sessions_dir = Path:new(vim.fn.stdpath 'data', 'sessions'), -- The directory where the session files will be saved.
-      autoload_mode = config.AutoloadMode.CurrentDir, -- Define what to do when Neovim is started without arguments.
-      autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
+      autoload_mode = config.AutoloadMode.CurrentDir,             -- Define what to do when Neovim is started without arguments.
+      autosave_ignore_not_normal = true,                          -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
       autosave_ignore_dirs = {
         '~',
       }, -- A list of directories where the session will not be autosaved.
@@ -15,10 +15,7 @@ return {
       --   'gitcommit',
       --   'gitrebase',
       -- },
-      -- autosave_ignore_buftypes = {}, -- All buffers of these bufer types will be closed before the session is saved.
-      -- autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
       max_path_length = 0, -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
-      -- load_include_current = false, -- The currently loaded session appears in the load_session UI.
     }
 
     vim.keymap.set('n', '<leader>sl', manager.load_session, { desc = '[S]ession [L]oad' })
