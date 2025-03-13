e6c0a5c8253055bd02fc3fb0f3a467adbe502594
Quan Teng Foong <e0564970@u.nus.edu>
2025-03-13 22:15:09 +0800

    add harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+return {
+  'ThePrimeagen/harpoon',
+  config = function()
+    local mark = require('harpoon.mark')
+    local ui = require('harpoon.ui')
+
+    vim.keymap.set('n', '<leader>a', mark.add_file)
+    vim.keymap.set('n', '<leader>fh', ui.toggle_quick_menu, { desc = '[F]ind [H]arpoon marks' })
+
+    vim.keymap.set('n', '<C-n>', function() ui.nav_file(1) end)
+    vim.keymap.set('n', '<C-m>', function() ui.nav_file(2) end)
+    vim.keymap.set('n', '<C-,>', function() ui.nav_file(3) end)
+    vim.keymap.set('n', '<C-.>', function() ui.nav_file(4) end)
+    vim.keymap.set('n', '<C-/>', function() ui.nav_file(5) end)
+  end
+}
