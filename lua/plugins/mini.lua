8fec2bdeaedce7e03f60a5be708076440e0845f4
Quan Teng Foong <tengfoong.quan@shopee.com>
2025-03-23 23:27:18 +0800

    add mini.animate toggle keymap with persistence

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,60 +1,76 @@
 -- TODO: consider using other plugins from mini
+-- TODO: customise statusline
 return { -- Collection of various small independent plugins/modules
-  'echasnovski/mini.nvim',
-  config = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
-    require('mini.ai').setup { n_lines = 500 }
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
 
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
-    local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
-    statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
 
-    -- You can configure sections in the statusline by overriding their
-    -- default behavior. For example, here we set the section for
-    -- cursor location to LINE:COLUMN
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
     end
+  },
+  {
+    'echasnovski/mini.animate',
+    config = function()
+      local persistence = require('utils.persistence')
+      local ani_filename = 'minianimate'
 
-    --
-    -- Animation for vim movements
-    --
-    local animate = require 'mini.animate'
-    animate.setup {
-      scroll = {
-        timing = animate.gen_timing.linear { duration = 5, unit = 'step' },
-        subscroll = animate.gen_subscroll.equal { max_output_steps = 120 }, -- number of steps in animation (smoothness)
-      },
-    }
+      local function enable_animation()
+        local animate = require 'mini.animate'
+        animate.setup {
+          scroll = {
+            timing = animate.gen_timing.linear { duration = 5, unit = 'step' },
+            subscroll = animate.gen_subscroll.equal { max_output_steps = 120 },
+          },
+        }
+        vim.g.minianimate_disable = false
+        persistence.save_state(ani_filename, true)
+      end
 
-    -- Disable by default
-    vim.g.minianimate_disable = true
+      local function disable_animation()
+        vim.g.minianimate_disable = true
+        persistence.save_state('minianimate', false)
+      end
 
-    vim.keymap.set('n', '<leader>ae', function()
-      vim.g.minianimate_disable = false
-    end, { desc = '[a]nimate [e]nable' })
+      vim.keymap.set('n', '<leader>anm', function()
+        if vim.g.minianimate_disable then
+          enable_animation()
+        else
+          disable_animation()
+        end
+      end, { desc = '[an]i[m]ate' })
 
-    vim.keymap.set('n', '<leader>ad', function()
-      vim.g.minianimate_disable = true
-    end, { desc = '[a]nimate [d]isable' })
-
-    -- ... and there is more!
-    --  Check out: https://github.com/echasnovski/mini.nvim
-  end,
+      if persistence.load_state(ani_filename, false) then
+        enable_animation()
+      else
+        disable_animation()
+      end
+    end
+  }
 }
